d517adbd4ed02cea0d3599b2cd6fb407ca7ff836
armanzulfikri <armanzulfikri20@gmail.com>
2021-02-16 11:10:43 +0700

    deploy

--- config/config.go
+++ config/config.go
@@ -2,40 +2,37 @@ package config
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Connect() *gorm.DB {
-	var userDatabase, passDatabase, portDatabase,
-		hostDatabase, nameDatabase, sslDatabase, timezoneDatabase string
+	// var userDatabase, passDatabase, portDatabase,
+	// 	hostDatabase, nameDatabase, sslDatabase, timezoneDatabase string
 
-	if err := godotenv.Load(".env"); err != nil {
-		log.Fatal("Error Load .env file")
-	} else {
-		userDatabase = os.Getenv("DATABASE_USER")
-		passDatabase = os.Getenv("DATABASE_PASS")
-		portDatabase = os.Getenv("DATABASE_PORT")
-		hostDatabase = os.Getenv("DATABASE_HOST")
-		nameDatabase = os.Getenv("DATABASE_NAME")
-		sslDatabase = os.Getenv("DATABASE_SSLMODE")
-		timezoneDatabase = os.Getenv("DATABASE_TIMEZONE")
+	// if err := godotenv.Load(".env"); err != nil {
+	// 	log.Fatal("Error Load .env file")
+	// } else {
+	// 	userDatabase = os.Getenv("DATABASE_USER")
+	// 	passDatabase = os.Getenv("DATABASE_PASS")
+	// 	portDatabase = os.Getenv("DATABASE_PORT")
+	// 	hostDatabase = os.Getenv("DATABASE_HOST")
+	// 	nameDatabase = os.Getenv("DATABASE_NAME")
+	// 	sslDatabase = os.Getenv("DATABASE_SSLMODE")
+	// 	timezoneDatabase = os.Getenv("DATABASE_TIMEZONE")
 
-	}
-	conn :=
-		" host=" + hostDatabase +
-			" user=" + userDatabase +
-			" password=" + passDatabase +
-			" dbname=" + nameDatabase +
-			" port=" + portDatabase +
-			" sslmode=" + sslDatabase +
-			" Timezone=" + timezoneDatabase
+	// }
+	// conn :=
+	// 	" host=" + hostDatabase +
+	// 		" user=" + userDatabase +
+	// 		" password=" + passDatabase +
+	// 		" dbname=" + nameDatabase +
+	// 		" port=" + portDatabase +
+	// 		" sslmode=" + sslDatabase +
+	// 		" Timezone=" + timezoneDatabase
 
-	db, errConn := gorm.Open(postgres.Open(conn), &gorm.Config{})
+	db, errConn := gorm.Open(postgres.Open("postgres://xkloddfddainoq:5070b9f51e9b823e71bcf485efba8735fa374494263e6bd1b9f0207de95dc50e@ec2-3-213-85-90.compute-1.amazonaws.com:5432/d3457tu1vpejgm"), &gorm.Config{})
 	if errConn != nil {
 		panic("failed to connect to database")
 	} else {
